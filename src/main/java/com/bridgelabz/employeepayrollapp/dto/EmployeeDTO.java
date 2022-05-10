ca5f42320ef3dca5ceeedbe2d343e5f9c3026e74
rutuja-nandiwadekar <nandiwadekarrutuja@gmail.com>
2022-05-10 12:52:20 +0530

    Added: Code to add remaining properties to the Payroll DTO and Model

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeeDTO.java
@@ -1,10 +1,14 @@
 package com.bridgelabz.employeepayrollapp.dto;
 
+import lombok.ToString;
+
 import javax.validation.constraints.Min;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
+import java.time.LocalDate;
+import java.util.List;
 
-public class EmployeeDTO {
+public @ToString class EmployeeDTO {
 
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$",
             message = "Employee name is Invalid")
@@ -13,7 +17,18 @@ public class EmployeeDTO {
     public String name;
 
     @Min(value = 500, message = "Min wage should be more than 500")
+
     public long salary;
+
+    public LocalDate startDate;
+
+    public String gender;
+
+    public String note;
+
+    public String profilePic;
+
+    public List<String> department;
 
 
     public EmployeeDTO(String name, long salary) {
--- src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/model/EmployeePayrollData.java
@@ -1,41 +1,34 @@
 package com.bridgelabz.employeepayrollapp.model;
 
 import com.bridgelabz.employeepayrollapp.dto.EmployeeDTO;
+import lombok.Data;
 
-public class EmployeePayrollData {
+import java.time.LocalDate;
+import java.util.List;
+
+public @Data class EmployeePayrollData {
 
     private int employeeId;
     private String name;
     private long salary;
+    private String gender;
+    private LocalDate startDate;
+    private String note;
+    private String profilePic;
+    private List<String> department;
+
 
     public EmployeePayrollData(int employeeId, EmployeeDTO employeeDTO) {
         this.employeeId = employeeId;
         this.name = employeeDTO.name;
         this.salary = employeeDTO.salary;
+        this.gender= employeeDTO.gender;
+        this.startDate= employeeDTO.startDate;
+        this.note= employeeDTO.note;
+        this.profilePic= employeeDTO.profilePic;
+        this.department = employeeDTO.department;
     }
 
 
-    public int getEmployeeId() {
-        return employeeId;
-    }
 
-    public void setEmployeeId(int employeeId) {
-        this.employeeId = employeeId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public long getSalary() {
-        return salary;
-    }
-
-    public void setSalary(long salary) {
-        this.salary = salary;
-    }
 }
